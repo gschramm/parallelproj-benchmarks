962ade77490a3d8c2e10679ee71992d3ab4f39d3
Georg Schramm <40211162+gschramm@users.noreply.github.com>
2023-09-30 20:27:45 +0200

    add implementation of regularpolygon coinc_desc

--- scanners.py
+++ scanners.py
@@ -5,6 +5,7 @@ import abc
 import array_api_compat.numpy as np
 import numpy.typing as npt
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d.art3d import Line3DCollection
 
 from types import ModuleType
 from array_api_compat import to_device, size
@@ -639,196 +640,180 @@ class GEDiscoveryMI(RegularPolygonPETScannerGeometry):
                          ring_positions=ring_positions,
                          symmetry_axis=symmetry_axis)
 
+class PETCoincidenceDescriptor(abc.ABC):
+    """abstract base class to describe which modules / indices in modules of a 
+       modularized PET scanner are in coincidence; defining geometrical LORs"""
+
+    def __init__(self,
+                 scanner: ModularizedPETScannerGeometry) -> None:
+        """
+        Parameters
+        ----------
+        scanner : ModularizedPETScannerGeometry
+            a modularized PET scanner 
+        """
+        self._scanner = scanner
+
+    @property
+    def scanner(self) -> ModularizedPETScannerGeometry:
+        """the scanner for which coincidences are described"""
+        return self._scanner
+
+    @property
+    def xp(self) -> ModuleType:
+        """array module to use for storing the LOR endpoints"""
+        return self.scanner.xp
+
+    @property
+    def dev(self) -> str:
+        """device to use for storing the LOR endpoints"""
+        return self.scanner.dev
+
+
+
+
+class RegularPolygonPETCoincidenceDescriptor(PETCoincidenceDescriptor):
+
+    def __init__(
+        self,
+        scanner: RegularPolygonPETScannerGeometry,
+        radial_trim: int = 3,
+        max_ring_difference: int | None = None,
+        sinogram_spatial_axis_order:
+        SinogramSpatialAxisOrder = SinogramSpatialAxisOrder.RVP
+    ) -> None:
+        """Coincidence descriptor for a regular polygon PET scanner where
+           we have coincidences within and between "rings (polygons of modules)" 
+           The geometrical LORs can be sorted into a sinogram having a
+           "plane", "view" and "radial" axis.
+
+        Parameters
+        ----------
+        scanner : RegularPolygonPETScannerGeometry
+            a regular polygon PET scanner
+        radial_trim : int, optional
+            number of geometrial LORs to disregard in the radial direction, by default 3
+        max_ring_difference : int | None, optional
+            maximim ring difference to consider for coincidences, by default None means
+            all ring differences are included
+        sinogram_spatial_axis_order : SinogramSpatialAxisOrder, optional
+            order of the spatial axis in the sinogram, by default SinogramSpatialAxisOrder.RVP
+            which means "planes", "views", "radial"
+        """
+
+        super().__init__(scanner)
+
+        self._radial_trim = radial_trim
+
+        if max_ring_difference is None:
+            self._max_ring_difference = self.scanner.num_rings - 1
+        else:
+            self._max_ring_difference = max_ring_difference
+
+        self._num_rad = (self.scanner.num_lor_endpoints_per_ring +
+                         1) - 2 * self._radial_trim
+        self._num_views = self.scanner.num_lor_endpoints_per_ring // 2
+
+        self._sinogram_spatial_axis_order = sinogram_spatial_axis_order
+
+        self.setup_plane_indices()
+        self.setup_view_indices()
+
+    def setup_plane_indices(self) -> None:
+        """setup the start / end plane indices (similar to a Michelogram)
+        """
+        self._start_plane_index = self.xp.arange(self.scanner.num_rings,
+                                            dtype=self.xp.int32, device=self.dev)
+        self._end_plane_index = self.xp.arange(self.scanner.num_rings,
+                                          dtype=self.xp.int32, device=self.dev)
+
+        for i in range(1, self._max_ring_difference + 1):
+            tmp1 = self.xp.arange(self.scanner.num_rings - i, dtype=self.xp.int16, device=self.dev)
+            tmp2 = self.xp.arange(self.scanner.num_rings - i, dtype=self.xp.int16, device=self.dev) + i
+
+            self._start_plane_index = self.xp.concat(
+                (self._start_plane_index, tmp1, tmp2))
+            self._end_plane_index = self.xp.concat(
+                (self._end_plane_index, tmp2, tmp1))
+
+        self._num_planes = self._start_plane_index.shape[0]
+
+    def setup_view_indices(self) -> None:
+        """setup the start / end view indices
+        """
+        n = self.scanner.num_lor_endpoints_per_ring
+
+        m = 2*(n//2)
+
+        self._start_in_ring_index = self.xp.zeros((self._num_views, self._num_rad),
+                                             dtype=self.xp.int32, device = self.dev)
+        self._end_in_ring_index = self.xp.zeros((self._num_views, self._num_rad),
+                                           dtype=self.xp.int32, device = self.dev)
+
+        for view in np.arange(self._num_views):
+            self._start_in_ring_index[view, :] = (
+                self.xp.concat((self.xp.arange(m) // 2, self.xp.asarray([n // 2]))) -
+                view)[self._radial_trim:-self._radial_trim]
+            self._end_in_ring_index[view, :] = (
+                self.xp.concat((self.xp.asarray([-1]), 
+                                     -((self.xp.arange(m) + 4) // 2))) -
+                view)[self._radial_trim:-self._radial_trim]
+
+        # shift the negative indices
+        self._start_in_ring_index = self.xp.where(self._start_in_ring_index >= 0, self._start_in_ring_index, self._start_in_ring_index + n)
+        self._end_in_ring_index = self.xp.where(self._end_in_ring_index >= 0, self._end_in_ring_index, self._end_in_ring_index + n)
+
+    def show_view(self,
+                  ax: plt.Axes,
+                  view: int,
+                  plane: int,
+                  lw: float = 0.2,
+                  **kwargs) -> None:
+        """show all LORs of a single view in a given plane
+
+        Parameters
+        ----------
+        ax : plt.Axes
+            a 3D matplotlib axes
+        view : int
+            the view number
+        plane : int
+            the plane number
+        lw : float, optional
+            the line width, by default 0.2
+        """
+
+        start_inds = self._start_in_ring_index[view, :]
+        end_inds = self._end_in_ring_index[view, :]
+
+        start_ring = self.xp.full(start_inds.shape, int(self._start_plane_index[plane]))
+        end_ring = self.xp.full(end_inds.shape, int(self._end_plane_index[plane]))
+
+        p1s = np.asarray(to_device(self.scanner.get_lor_endpoints(start_ring, start_inds), 'cpu'))
+        p2s = np.asarray(to_device(self.scanner.get_lor_endpoints(end_ring, end_inds), 'cpu'))
+
+        ls = np.hstack([p1s, p2s]).copy()
+        ls = ls.reshape((-1, 2, 3))
+        lc = Line3DCollection(ls, linewidths=lw, **kwargs)
+        ax.add_collection(lc)
+
+
 
 if __name__ == '__main__':
-    #import numpy.array_api as xp
-    #dev = 'cpu'
+    import numpy.array_api as xp
     #import array_api_compat.torch as xp
-    import array_api_compat.cupy as xp
-    dev = 'cuda'
+    #import array_api_compat.cupy as xp
 
-    geom = GEDiscoveryMI(xp, dev)
+    dev = 'cpu'
+    #dev = 'cuda'
+
+    scanner = GEDiscoveryMI(xp, dev, num_rings=9)
+    coinc_desc = RegularPolygonPETCoincidenceDescriptor(scanner, radial_trim=87)
 
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
-    geom.show_lor_endpoints(ax)
+    scanner.show_lor_endpoints(ax)
+    coinc_desc.show_view(ax, 0, 0, lw=0.1, color='b')
+    coinc_desc.show_view(ax, 50, 0, lw=0.1, color='r')
     fig.show()
 
-#class PETCoincidenceDescriptor(abc.ABC):
-#    """abstract base class to describe which modules / indices in modules of a
-#       modularized PET scanner are in coincidence; defining geometrical LORs"""
-#
-#    def __init__(self,
-#                 scanner: scanners.ModularizedPETScannerGeometry) -> None:
-#        """
-#        Parameters
-#        ----------
-#        scanner : ModularizedPETScannerGeometry
-#            a modularized PET scanner
-#        """
-#        self._scanner = scanner
-#
-#    @property
-#    def scanner(self) -> scanners.ModularizedPETScannerGeometry:
-#        """the scanner for which coincidences are described"""
-#        return self._scanner
-#
-#    #-------------------------------------------------------------------
-#    #-------------------------------------------------------------------
-#    # abstract methods
-#
-#    @property
-#    @abc.abstractmethod
-#    def num_lors(self) -> int:
-#        """the total number of geometrical LORs
-#        """
-#        raise NotImplementedError
-#
-#    @abc.abstractmethod
-#    def get_modules_and_indices_in_coincidence(
-#            self, module: int, index_in_module: int) -> npt.NDArray:
-#        """ return (N,2) array of two integers showing which module/index_in_module combinations
-#            are in coincidence with the given input module / index_in_module
-#
-#        Parameters
-#        ----------
-#        module : int
-#            the module number
-#        index_in_module : int
-#            the (LOR endpoint) index in the module
-#
-#        Returns
-#        -------
-#        npt.NDArray
-#            (N,2) array of two integers showing which module/index_in_module
-#        """
-#
-#    @abc.abstractmethod
-#    def get_lor_indices(
-#        self,
-#        linear_lor_indices: None | slice | npt.NDArray = None
-#    ) -> tuple[npt.NDArray, npt.NDArray, npt.NDArray, npt.NDArray]:
-#        """ mapping that maps the linear LOR index to the 4 1D arrays
-#            representing start_module, start_index_in_module, end_module,
-#            end_index_in_module
-#
-#        Parameters
-#        ----------
-#        linear_lor_indices : None | slice | npt.NDArray, optional
-#            containing the linear (flattened) indices of geometrical LORs, by default None
-#
-#        Returns
-#        -------
-#        tuple[npt.NDArray, npt.NDArray, npt.NDArray, npt.NDArray]
-#            start_module, start_index_in_module, end_module, end_index_in_module
-#        """
-#
-#    #-------------------------------------------------------------------
-#    #-------------------------------------------------------------------
-#
-#    def setup_lor_lookup_table(self) -> None:
-#        """setup a lookup table for the start and end modules / indecies in module for all
-#           geometrical LORs
-#        """
-#        for mod, num_lor_endpoints in enumerate(
-#                self.scanner.num_lor_endpoints_per_module):
-#            for lor in range(num_lor_endpoints):
-#                tmp = self.get_modules_and_indices_in_coincidence(mod, lor)
-#                # make sure we do not LORs twice
-#                tmp = tmp[tmp[:, 0] >= mod]
-#
-#                if mod == 0 and lor == 0:
-#                    self._lor_start_module_index = np.repeat(np.array(
-#                        [[mod, lor]], dtype=np.uint16),
-#                                                             tmp.shape[0],
-#                                                             axis=0)
-#                    self._lor_end_module_index = tmp.copy().astype(np.uint16)
-#                else:
-#                    self._lor_start_module_index = np.vstack(
-#                        (self._lor_start_module_index,
-#                         np.repeat(np.array([[mod, lor]]),
-#                                   tmp.shape[0],
-#                                   axis=0)))
-#                    self._lor_end_module_index = np.vstack(
-#                        (self._lor_end_module_index, tmp))
-#
-#    def show_all_lors_for_endpoint(self,
-#                                   ax: plt.Axes,
-#                                   module: int,
-#                                   index_in_module: int,
-#                                   lw: float = 0.2,
-#                                   **kwargs) -> None:
-#        """show all geometrical LORs for a given LOR endpoint
-#
-#        Parameters
-#        ----------
-#        ax : plt.Axes
-#            a 3D matplotlib axes
-#        module : int
-#            the module number
-#        index_in_module : int
-#            the index in the module
-#        lw : float, optional
-#            line width, by default 0.2
-#        **kwargs :
-#            keyword arguments passed to Line3DCollection
-#        """
-#
-#        tmp = self.get_modules_and_indices_in_coincidence(
-#            module, index_in_module)
-#
-#        end_mod = tmp[:, 0]
-#        end_ind = tmp[:, 1]
-#
-#        start_mod = np.full(end_mod.shape[0], module)
-#        start_ind = np.full(end_mod.shape[0], index_in_module)
-#
-#        p1s = self.scanner.get_lor_endpoints(start_mod, start_ind)
-#        p2s = self.scanner.get_lor_endpoints(end_mod, end_ind)
-#
-#        # get_lor_endpoints can return a numpy or cupy array
-#        # it scanner uses cupy arrays, we have to convert them into numpy arrays
-#        if self.scanner.xp.__name__ == 'cupy':
-#            p1s = self.scanner.xp.asnumpy(p1s)
-#            p2s = self.scanner.xp.asnumpy(p2s)
-#
-#        ls = np.hstack([p1s, p2s]).copy()
-#        ls = ls.reshape((-1, 2, 3))
-#        lc = Line3DCollection(ls, linewidths=lw, **kwargs)
-#        ax.add_collection(lc)
-#
-#    def show_lors(self,
-#                  ax: plt.Axes,
-#                  lors: None | npt.NDArray,
-#                  lw: float = 0.2,
-#                  **kwargs) -> None:
-#        """show a given set of LORs
-#
-#        Parameters
-#        ----------
-#        ax : plt.Axes
-#            a 3D matplotlib axes
-#        lors : None | npt.NDArray
-#            the linear (flattened) index of the geometrical LORs to show
-#            None means all geometrical LORs are shown
-#        lw : float, optional
-#            linewidth, by default 0.2
-#        **kwargs :
-#            keyword arguments passed to Line3DCollection
-#        """
-#        start_mod, start_ind, end_mod, end_ind = self.get_lor_indices(lors)
-#        p1s = self.scanner.get_lor_endpoints(start_mod, start_ind)
-#        p2s = self.scanner.get_lor_endpoints(end_mod, end_ind)
-#
-#        # get_lor_endpoints can return a numpy or cupy array
-#        # it scanner uses cupy arrays, we have to convert them into numpy arrays
-#        if self.scanner.xp.__name__ == 'cupy':
-#            p1s = self.scanner.xp.asnumpy(p1s)
-#            p2s = self.scanner.xp.asnumpy(p2s)
-#
-#        ls = np.hstack([p1s, p2s]).copy()
-#        ls = ls.reshape((-1, 2, 3))
-#        lc = Line3DCollection(ls, linewidths=lw, **kwargs)
-#        ax.add_collection(lc)
-#
-#
