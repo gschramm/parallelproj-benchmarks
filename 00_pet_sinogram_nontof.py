b2aca1f052e687deafac4ad9d389d44afe5a121a
Georg <gschra2@stanford.edu>
2023-10-02 00:15:58 -0700

    improve 00 benchmarck

--- 00_pet_sinogram_nontof.py
+++ 00_pet_sinogram_nontof.py
@@ -10,10 +10,17 @@ from pathlib import Path
 parser = argparse.ArgumentParser()
 parser.add_argument('--num_runs', type=int, default=5)
 parser.add_argument('--num_subsets', type=int, default=34)
-parser.add_argument('--mode', default='GPU', choices=['GPU', 'GPU-torch', 'CPU', 'CPU-torch', 'hybrid'])
+parser.add_argument('--mode',
+                    default='GPU',
+                    choices=['GPU', 'GPU-torch', 'CPU', 'CPU-torch', 'hybrid'])
 parser.add_argument('--threadsperblock', type=int, default=32)
 parser.add_argument('--output_file', type=int, default=None)
 parser.add_argument('--output_dir', default='results')
+parser.add_argument('--sinogram_orders',
+                    default=['PVR', 'PRV', 'VPR', 'VRP', 'RPV', 'RVP'],
+                    nargs='+')
+parser.add_argument('--symmetry_axes', default=['0', '1', '2'], nargs='+')
+
 args = parser.parse_args()
 
 if args.mode == 'GPU':
@@ -45,7 +52,6 @@ elif args.mode == 'CPU-torch':
 else:
     raise ValueError
 
-
 num_runs = args.num_runs
 threadsperblock = args.threadsperblock
 num_subsets = args.num_subsets
@@ -57,22 +63,22 @@ if args.output_file is None:
 # image properties
 num_trans = 215
 num_ax = 71
-voxel_size = xp.asarray([2.78, 2.78, 2.78], dtype=xp.float32, device = dev)
+voxel_size = xp.asarray([2.78, 2.78, 2.78], dtype=xp.float32, device=dev)
 
 # scanner properties
 num_rings = 36
 
-#---------------------------------------------------------------------
-#sinogram_orders = ('PVR', 'PRV', 'VPR', 'VRP', 'RPV', 'RVP')
-#symmetry_axes = (0, 1, 2)
+sinogram_orders = args.sinogram_orders
+symmetry_axes = [int(x) for x in args.symmetry_axes]
 
-sinogram_orders = ('PVR', 'PRV')
-symmetry_axes = (0, 2)
+#---------------------------------------------------------------------
 
 df = pd.DataFrame()
 
 for ia, symmetry_axis in enumerate(symmetry_axes):
-    lor_descriptor = scanners.GEDiscoveryMILORDescriptor(xp, dev, symmetry_axis=2)
+    lor_descriptor = scanners.GEDiscoveryMILORDescriptor(xp,
+                                                         dev,
+                                                         symmetry_axis=2)
     views = xp.arange(0, lor_descriptor.num_views, num_subsets, device=dev)
 
     # setup a box like test image
@@ -82,7 +88,7 @@ for ia, symmetry_axis in enumerate(symmetry_axes):
     n0, n1, n2 = img_shape
 
     # setup an image containing a square
-    img = xp.zeros(img_shape, dtype=xp.float32, device = dev)
+    img = xp.zeros(img_shape, dtype=xp.float32, device=dev)
     sl = [
         slice(n0 // 4, 3 * n0 // 4, None),
         slice(n1 // 4, 3 * n1 // 4, None),
@@ -94,7 +100,7 @@ for ia, symmetry_axis in enumerate(symmetry_axes):
     img[sl] = 1
 
     # setup the image origin = the coordinate of the [0,0,0] voxel
-    img_origin = (-(xp.asarray(img.shape, dtype=xp.float32, device = dev) / 2) +
+    img_origin = (-(xp.asarray(img.shape, dtype=xp.float32, device=dev) / 2) +
                   0.5) * voxel_size
 
     for io, sinogram_order in enumerate(sinogram_orders):
@@ -108,39 +114,36 @@ for ia, symmetry_axis in enumerate(symmetry_axes):
         for ir in range(num_runs + 1):
             # perform a complete fwd projection
             t0 = time.time()
-            img_fwd = parallelproj.joseph3d_fwd(xstart,
-                                      xend,
-                                      img,
-                                      img_origin,
-                                      voxel_size,
-                                      threadsperblock=threadsperblock)
+            img_fwd = parallelproj.joseph3d_fwd(
+                xstart,
+                xend,
+                img,
+                img_origin,
+                voxel_size,
+                threadsperblock=threadsperblock)
             t1 = time.time()
 
             # perform a complete backprojection
             back_img = xp.zeros(img.shape, dtype=xp.float32)
             ones = xp.ones(img_fwd.shape, dtype=xp.float32)
             t2 = time.time()
-            back_img = parallelproj.joseph3d_back(xstart,
-                                       xend,
-                                       img_shape,
-                                       img_origin,
-                                       voxel_size,
-                                       ones,
-                                       threadsperblock=threadsperblock)
+            back_img = parallelproj.joseph3d_back(
+                xstart,
+                xend,
+                img_shape,
+                img_origin,
+                voxel_size,
+                ones,
+                threadsperblock=threadsperblock)
             t3 = time.time()
             if ir > 0:
                 tmp = pd.DataFrame(
                     {
-                        'sinogram order':
-                        sinogram_order,
-                        'symmetry axis':
-                        str(symmetry_axis),
-                        'run':
-                        ir,
-                        't forward (s)':
-                        t1 - t0,
-                        't back (s)':
-                        t3 - t2
+                        'sinogram order': sinogram_order,
+                        'symmetry axis': str(symmetry_axis),
+                        'run': ir,
+                        't forward (s)': t1 - t0,
+                        't back (s)': t3 - t2
                     },
                     index=[0])
                 df = pd.concat((df, tmp))
@@ -155,16 +158,16 @@ df['threadsperblock'] = threadsperblock
 Path(output_dir).mkdir(exist_ok=True, parents=True)
 df.to_csv(os.path.join(output_dir, output_file), index=False)
 
-
 #----------------------------------------------------------------------------
 # show results
 import matplotlib.pyplot as plt
 import seaborn as sns
+
 sns.set_context('paper')
 
 df['t forward+back (s)'] = df['t forward (s)'] + df['t back (s)']
 
-fig, ax = plt.subplots(1, 3, figsize=(7, 7/3), sharex=False, sharey='row')
+fig, ax = plt.subplots(1, 3, figsize=(7, 7 / 3), sharex=False, sharey='row')
 
 bplot_kwargs = dict(capsize=0.15, errwidth=1.5, errorbar='sd')
 
@@ -194,4 +197,3 @@ for i, axx in enumerate(ax.ravel()):
         axx.get_legend().remove()
 
 fig.show()
-
